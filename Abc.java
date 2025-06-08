fe74fe10021dcf7dd5d96026b72de48340b48d03
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2025-06-08 15:22:29 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,5 +1,5 @@
 public class Abc {
     public static void main(String[] args) {
-        System.out.println("Hello World from Devanshu Shah");
+        System.out.println("Hello World Devanshu Shah");
     }
 }
